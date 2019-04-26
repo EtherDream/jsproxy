96f4cd1f71253a05b68f05b1d90baa41c6fa6c37
zjcqoo <zjcqoo@gmail.com>
2019-04-26 14:48:49 +0800

    update

--- upload.sh
+++ upload.sh
@@ -2,12 +2,13 @@ HOST=etherdream.com
 NODE=(
   node-aliyun-hk
   node-aliyun-sg
+  node-bwh-los
 )
 for v in ${NODE[@]}; do
   echo "$v upload ..."
 
   rsync . jsproxy@$v.$HOST:server \
-    --delete -r \
+    -r \
     --exclude='nginx/cache/*' \
     --exclude='nginx/logs/*'
 
