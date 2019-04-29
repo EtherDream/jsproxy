b5515add9f123d03f1e69790eae57f3d461c317d
zjcqoo <zjcqoo@gmail.com>
2019-04-29 17:49:16 +0800

    上传保留文件属性

--- upload.sh
+++ upload.sh
@@ -1,13 +1,17 @@
+#!/usr/bin/env bash
+# 功能：同步文件到所有节点，并重启服务
+
 HOST=etherdream.com
 NODE=(
   node-aliyun-hk
   node-aliyun-sg
+  node-bwh-los
 )
 for v in ${NODE[@]}; do
   echo "$v upload ..."
 
   rsync . jsproxy@$v.$HOST:server \
-    --delete -r \
+    -r -p \
     --exclude='nginx/cache/*' \
     --exclude='nginx/logs/*'
 
