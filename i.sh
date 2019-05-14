fbb537d5ead2e7ae5fbcae8c3664889b863396e8
zjcqoo <zjcqoo@gmail.com>
2019-05-14 17:14:34 +0800

    一键安装脚本更新

--- i.sh
+++ i.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env sh
 
-CDN=https://cdn.jsdelivr.net/gh/etherdream/jsproxy-bin@master/dist/
 DST=/home/jsproxy/setup.sh
-
 
 groupadd nobody
 useradd jsproxy -g nobody --create-home
 
-curl -s "$CDN/setup.sh" -o $DST
+echo "download main script ..."
+curl -s "https://raw.githubusercontent.com/EtherDream/jsproxy-bin/master/setup.sh" -o $DST
 chmod +x $DST
 
 su - jsproxy -c $DST
