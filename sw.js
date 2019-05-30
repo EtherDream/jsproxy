ae50d5af2d2d79c63b0df98ba811977a9cc38db6
zjcqoo <zjcqoo@gmail.com>
2019-05-30 11:34:17 +0800

    sync www files

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-importScripts(__FILE__='https://cdn.jsdelivr.net/gh/zjcqoo/zjcqoo.github.io@master/assets/bundle.b30fe48b.js')
+importScripts(__FILE__='https://cdn.jsdelivr.net/gh/etherdream/jsproxy@gh-pages/assets/bundle.b30fe48b.js')
