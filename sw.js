15bd0ab842665c97f0ba1f3f366fc70ac05b5391
zjcqoo <zjcqoo@gmail.com>
2019-06-23 20:35:04 +0800

    修复 状态码为空的问题

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-jsproxy_config=x=>{__CONF__=x;importScripts(__FILE__=x.assets_cdn+'bundle.2030f543.js')};importScripts('conf.js')
+jsproxy_config=x=>{__CONF__=x;importScripts(__FILE__=x.assets_cdn+'bundle.6fecf114.js')};importScripts('conf.js')
