682470584319b0bb036ede74bbff21d4e417b343
zjcqoo <zjcqoo@gmail.com>
2019-07-27 17:27:05 +0800

    更新 conf.js 默认配置

--- conf.js
+++ conf.js
@@ -2,12 +2,12 @@ jsproxy_config({
   // 当前配置的版本（记录在日志中，用于排查问题）
   // 每次修改配置，该值需要增加，否则不会生效。
   // 默认每隔 5 分钟自动下载配置，若想立即验证，可通过隐私模式访问。
-  ver: '107',
+  ver: '110',
 
   // 通过 CDN 加速常用网站的静态资源（实验中）
   static_boost: {
     enable: true,
-    ver: 60
+    ver: 62
   },
 
   // 节点配置
@@ -16,9 +16,8 @@ jsproxy_config({
       label: '演示服务-香港节点',
       lines: {
         // 主机:权重
-        'node-aliyun-hk-0.etherdream.com:8443': 1,
         'node-aliyun-hk-1.etherdream.com:8443': 1,
-        'node-aliyun-hk-2.etherdream.com:8443': 1,
+        'node-aliyun-hk-2.etherdream.com:8443': 2,
       }
     },
     'demo-sg': {
@@ -39,7 +38,7 @@ jsproxy_config({
       hidden: true,
       lines: {
         // 收费版（高权重）
-        'node-cfworker.etherdream.com': 4,
+        'node-cfworker-2.etherdream.com': 4,
 
         // 免费版（低权重，分摊一些成本）
         // 每个账号每天 10 万次免费请求，但有频率限制
@@ -54,8 +53,8 @@ jsproxy_config({
   /**
    * 默认节点
    */
-  // node_default: 'mysite',
-  node_default: /jsproxy-demo\.\w+$/.test(location.host) ? 'demo-hk' : 'mysite',
+  node_default: 'mysite',
+  // node_default: /jsproxy-demo\.\w+$/.test(location.host) ? 'demo-hk' : 'mysite',
 
   /**
    * 加速节点
@@ -66,10 +65,10 @@ jsproxy_config({
    * 静态资源 CDN 地址
    * 用于加速 `assets` 目录中的资源访问
    */
-  assets_cdn: 'https://cdn.jsdelivr.net/gh/zjcqoo/zjcqoo.github.io@master/assets/',
+  // assets_cdn: 'https://cdn.jsdelivr.net/gh/zjcqoo/zjcqoo.github.io@master/assets/',
 
   // 本地测试时打开，否则访问的是线上的
-  // assets_cdn: 'assets/',
+  assets_cdn: 'assets/',
 
   // 首页路径
   index_path: 'index_v3.html',
