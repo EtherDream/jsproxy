9cfaa59f1ef408c5a2bd4f35c75fe27b3b1706eb
EtherDream <zjcqoo@gmail.com>
2020-02-19 11:58:05 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -38,14 +38,14 @@ jsproxy_config({
       hidden: true,
       lines: {
         // 收费版（高权重）
-        'node-cfworker-2.etherdream.com': 4,
+        //'node-cfworker-2.etherdream.com': 4,
 
         // 免费版（低权重，分摊一些成本）
         // 每个账号每天 10 万次免费请求，但有频率限制
-        'b.007.workers.dev': 1,
-        'b.hehe.workers.dev': 1,
-        'b.lulu.workers.dev': 1,
-        'b.jsproxy.workers.dev': 1,
+        //'b.007.workers.dev': 1,
+        //'b.hehe.workers.dev': 1,
+        //'b.lulu.workers.dev': 1,
+        //'b.jsproxy.workers.dev': 1,
       }
     }
   },
