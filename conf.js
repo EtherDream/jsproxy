7b11f5df52a1f6bc20a6d2b661301a2520b16aa0
utcx <77433125+utcx@users.noreply.github.com>
2022-06-27 14:11:46 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -13,17 +13,17 @@ jsproxy_config({
   // 节点配置
   node_map: {
     'demo-hk': {
-      label: '演示服务-香港节点',
+      label: '演示服务-1',
       lines: {
         // 主机:权重
-        'node-aliyun-hk-1.etherdream.com:8443': 1,
-        'node-aliyun-hk-2.etherdream.com:8443': 2,
+        'domain.com': 1,
+        'domain.cn': 2,
       }
     },
     'demo-sg': {
-      label: '演示服务-新加坡节点',
+      label: '演示服务-2',
       lines: {
-        'node-aliyun-sg.etherdream.com:8443': 1,
+        'domain.com': 1,
       },
     },
     'mysite': {
